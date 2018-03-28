d540a9147d24b9d5d337a30aa4d5d65f2d522f34
Oliver Binns <mail@oliverbinns.co.uk>
2018-03-28 01:27:32 +0100

    Added initial unit testing setup.

---
+++ Testing/src/test/sim/field/SparseFieldTest.java
@@ -0,0 +1,11 @@
+package sim.field;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SparseFieldTest{
+	@Test
+	public void helloWorld(){
+		assertTrue("Hello World", true);
+	}
+}
